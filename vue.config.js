b7fa5a4265452c1b1d8bc54f669e81da8254eb70
wu241617 <1272689028@qq.com>
2020-05-03 10:27:34 +0800

    压缩版本

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,12 @@
 //反向代理:解决跨域问题
 module.exports = {
-	devServer:{
-		proxy:{
-			'/api':{
-				target:'http://39.97.33.178',
-				changeOrigin:true
-			}
-		}
-	}/*,
-	publicPath: './' */
-} 
+    devServer: {
+        proxy: {
+            '/api': {
+                target: 'http://39.97.33.178',
+                changeOrigin: true
+            }
+        }
+    },
+    publicPath: './'
+}
